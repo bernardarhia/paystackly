1d641fe28d6ad8e3970ab071dc9fd00469692f9e
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-08 19:44:31 +0100

    added fetch bank implementation

--- src/dedicatedVirtualAccount/DedicatedVirtualAccount.ts
+++ src/dedicatedVirtualAccount/DedicatedVirtualAccount.ts
@@ -1,5 +1,15 @@
-import { BaseDedicatedVirtualAccount } from "./type";
+import { Http } from "../core/Http";
+import {
+  BaseDedicatedVirtualAccount,
+  FetchBankProvidersResponse,
+} from "./type";
 
 export class DedicatedVirtualAccount extends BaseDedicatedVirtualAccount {
   private endpoint = "/dedicated_account";
+
+  async fetchBankProviders(): Promise<FetchBankProvidersResponse> {
+    return await Http.get<FetchBankProvidersResponse>(
+      `${this.endpoint}/available_providers`
+    );
+  }
 }
