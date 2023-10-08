cf93d6f9ef299d29162fe3c03e45865822acc7b5
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-08 21:16:07 +0100

    added type def for assigning DVA

--- src/dedicatedVirtualAccount/type.ts
+++ src/dedicatedVirtualAccount/type.ts
@@ -7,7 +7,9 @@ import {
 
 export abstract class BaseDedicatedVirtualAccount {
   abstract create(payload: any): Promise<any>;
-  abstract assign(payload: any): Promise<any>;
+  abstract assign(
+    payload: AssignDedicatedVirtualAccountPayload
+  ): Promise<AssignDedicatedVirtualAccountResponse>;
   abstract list(
     payload: ListDedicatedVirtualAccountParams
   ): Promise<ListDedicatedVirtualAccountResponse>;
@@ -221,3 +223,40 @@ export type ListDedicatedVirtualAccountResponse = BaseResponse<
   }[] &
     PaginationMetadata
 >;
+
+export interface AssignDedicatedVirtualAccountPayload {
+  /* Customer email address */
+  email: string;
+
+  /* Customer's first name */
+  first_name: string;
+
+  /* Customer's last name */
+  last_name: string;
+
+  /* Customer's phone number */
+  phone: string;
+
+  /* The bank slug for preferred bank. To get a list of available banks, use the List Banks endpoint, passing pay_with_bank_transfer=true query parameter */
+  preferred_bank: string;
+
+  /* Currently accepts NG only*/
+  country: "NG";
+
+  /* Customer's account number */
+  account_number?: string;
+
+  /* Customer's Bank Verification Number */
+  bvn?: string;
+
+  /* Customer's bank code */
+  bank_code?: string;
+
+  /* Subaccount code of the account you want to split the transaction with */
+  subaccount?: string;
+
+  /* Split code consisting of the lists of accounts you want to split the transaction with */
+  split_code?: string;
+}
+
+export type AssignDedicatedVirtualAccountResponse = BasePaystackResponse;
