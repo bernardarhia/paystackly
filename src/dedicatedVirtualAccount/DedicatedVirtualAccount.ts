3555b6bd32334522f220c55a7c49b779e4ec89bc
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-08 19:53:58 +0100

    added implementation for remove split transaction

--- src/dedicatedVirtualAccount/DedicatedVirtualAccount.ts
+++ src/dedicatedVirtualAccount/DedicatedVirtualAccount.ts
@@ -2,6 +2,8 @@ import { Http } from "../core/Http";
 import {
   BaseDedicatedVirtualAccount,
   FetchBankProvidersResponse,
+  RemoveSplitTransactionPayload,
+  RemoveSplitTransactionResponse,
 } from "./type";
 
 export class DedicatedVirtualAccount extends BaseDedicatedVirtualAccount {
@@ -11,5 +13,13 @@ export class DedicatedVirtualAccount extends BaseDedicatedVirtualAccount {
     return await Http.get<FetchBankProvidersResponse>(
       `${this.endpoint}/available_providers`
     );
+  }
+  async removeSplitTransaction(
+    payload: RemoveSplitTransactionPayload
+  ): Promise<RemoveSplitTransactionResponse> {
+    return await Http.delete<
+      RemoveSplitTransactionPayload,
+      RemoveSplitTransactionResponse
+    >(`${this.endpoint}/split`, payload);
   }
 }
