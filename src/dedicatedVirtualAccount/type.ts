f6cdb26114443ca19b64f313ca114ef513ce8fc3
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-08 20:13:46 +0100

    added type defs for split transaction response and payload

--- src/dedicatedVirtualAccount/type.ts
+++ src/dedicatedVirtualAccount/type.ts
@@ -7,7 +7,9 @@ export abstract class BaseDedicatedVirtualAccount {
   abstract fetch(params: any): Promise<any>;
   abstract queryAccount(params: any): Promise<any>;
   abstract deactivate(params: any): Promise<any>;
-  abstract splitTransaction(payload: any): Promise<any>;
+  abstract splitTransaction(
+    payload: SplitTransactionPayload
+  ): Promise<SplitTransactionResponse>;
   abstract removeSplitTransaction(
     payload: RemoveSplitTransactionPayload
   ): Promise<RemoveSplitTransactionResponse>;
@@ -38,4 +40,57 @@ export type RemoveSplitTransactionResponse = BaseResponse<{
   active: boolean;
   createdAt: string;
   updatedAt: string;
+}>;
+
+export interface SplitTransactionPayload {
+  /* Customer ID or code */
+  customer: string;
+
+  /* Subaccount code of the account you want to split the transaction with */
+  subaccount?: string;
+
+  /* Split code consisting of the lists of accounts you want to split the transaction with */
+  split_code?: string;
+
+  /* The bank slug for preferred bank. To get a list of available banks, use the List Providers endpoint */
+  preferred_bank?: string;
+}
+
+export type SplitTransactionResponse = BaseResponse<{
+  bank: {
+    name: string;
+    id: number;
+    slug: string;
+  };
+  account_name: string;
+  account_number: string;
+  assigned: boolean;
+  currency: string;
+  metadata: null | Object;
+  active: boolean;
+  id: number;
+  created_at: string;
+  updated_at: string;
+  assignment: {
+    integration: number;
+    assignee_id: number;
+    assignee_type: string;
+    expired: boolean;
+    account_type: string;
+    assigned_at: string;
+    expired_at: null | string;
+  };
+  split_config: {
+    split_code: string;
+  };
+  customer: {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    customer_code: string;
+    phone: null | string;
+    metadata: null | Object;
+    risk_action: string;
+  };
 }>;
