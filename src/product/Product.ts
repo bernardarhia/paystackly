c1eb3cfa14cd692dd4b25a9572f00bcd23ac69de
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-09 09:43:31 +0100

    added implementation for update product

--- src/product/Product.ts
+++ src/product/Product.ts
@@ -1,5 +1,21 @@
-import { BaseProduct } from ".";
+import { Http } from "../core/Http";
+import {
+  BaseProduct,
+  ProductIdParama,
+  ProductUpdatePayload,
+  ProductUpdateResponse,
+} from "./types";
 
 export class Product extends BaseProduct {
   private endpoint = "/product";
+
+  async update(
+    param: ProductIdParama,
+    payload: ProductUpdatePayload
+  ): Promise<ProductUpdateResponse> {
+    return Http.put<ProductUpdatePayload, ProductUpdateResponse>(
+      `${this.endpoint}/${param.id}`,
+      payload
+    );
+  }
 }
