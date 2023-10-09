39fe85829c6789b286c4586596782a7d9f6d13ef
Oliver Otchere <oliverotchere4@gmail.com>
2023-10-09 10:50:55 +0100

    added implementation for update plan

--- src/plan/Plan.ts
+++ src/plan/Plan.ts
@@ -1,5 +1,17 @@
-import { BasePlan } from "./type";
+import { Http } from "../core/Http";
+import { BasePaystackResponse } from "../types";
+import { BasePlan, PlanIdParam, PlanPayload, PlanUpdateResponse } from "./type";
 
 export class Plan extends BasePlan {
   private endpoint = "/plan";
+
+  async update(
+    param: PlanIdParam,
+    payload: PlanPayload
+  ): Promise<BasePaystackResponse> {
+    return Http.put<PlanPayload, PlanUpdateResponse>(
+      `${this.endpoint}/${param.id_or_code}`,
+      payload
+    );
+  }
 }
